871a3c8d7c5fdacd6e666f98aee9d55c75ab1f21
phammanhdung0508 <kibichan52@gmail.com>
2025-09-11 21:18:22 +0700

    update: cheat

--- src/interfaces/cli/CheatCommandHandler.ts
+++ src/interfaces/cli/CheatCommandHandler.ts
@@ -7,24 +7,28 @@ import { CheatUseCase } from "@/application/use-cases/CheatUseCase";
 
 export class CheatCommandHandler implements CommandHandler {
     constructor(private cheatPetUseCase: CheatUseCase,
-         private getPetsUseCase: GetPetsUseCase) { }
+        private getPetsUseCase: GetPetsUseCase) { }
     async handle(channel: TextChannel, message: Message, channelMsg?: ChannelMessage): Promise<void> {
         const opponentId = message.mentions?.[0]?.user_id;
-        
+
         const petatt = await this.getPetsUseCase.execute(message.sender_id);
         const petdef = await this.getPetsUseCase.execute(opponentId!);
 
         const peta = petatt[0]
         const petd = petdef[0]
 
-        const args = message.content.t?.slice(1).trim().split(/ +/);
+        // const args = message.content.t?.slice(1).trim().split(/ +/);
 
-        if (!args) return
+        // if (!args) return
 
-        args.shift(); // command
-        args.shift(); // subcommand
-    
-        await this.cheatPetUseCase.excute(message.sender_id, peta.id, Number(args[0]), Number(args[1]))
-        await this.cheatPetUseCase.excute(opponentId!, petd.id, Number(args[0]), Number(args[1]))
+        // args.shift(); // command
+        // args.shift(); // subcommand
+
+        const lev = [19, 39, 59, 99]
+
+        for (let index = 0; index < lev.length; index++) {
+            await this.cheatPetUseCase.excute(message.sender_id, peta.id, 100000000000, lev[index])
+            await this.cheatPetUseCase.excute(opponentId!, petd.id, 100000000000, lev[index])
+        }
     }
 }
